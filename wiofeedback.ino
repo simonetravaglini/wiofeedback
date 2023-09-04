00368dee6be7f1608e668d0c03f1ca4adc945021
simonetravaglini <simonetravaglini@gmail.com>
2023-09-04 22:43:18 +0200

    Update wiofeedback.ino
    added HR device

--- wiofeedback.ino
+++ wiofeedback.ino
@@ -16,9 +16,14 @@
 const unsigned int BATTERY_CAPACITY = 650; // Set Wio battery capacity
 
 File myFile;
+File listOfSessions;
 TFT_eSPI tft; // initialize TFT LCD
 TFT_eSprite spr = TFT_eSprite( & tft);
 
+String menuOfSessions[100]; 
+int valueCount = 0;  // Contatore delle stringhe lette 
+long int sessionsPosition = 0;
+int sessionsMax;
 boolean recording = false;
 const int GSR = A0;
 unsigned long previousMillis = 0;
@@ -26,10 +31,17 @@ boolean serialSending = false;
 int sensorValue = 0;
 int gsr_average = 0;
 int gsrIndex = 0;
+
 float ohm;
 float conductance;
 doubles gsr_data;
-int previousSoc = 0; // previous value of battery charge
+int previousSoc = 101; // previous value of battery charge
+
+const int max_heartpluse_duty = 2000;
+unsigned long valueHeartRate;
+float heartRate;
+doubles hr_data;
+int hrIndex = 0;
 
 /*SETUP*/
 
@@ -39,14 +51,22 @@ void setup() {
   pinMode(WIO_KEY_B, INPUT_PULLUP);
   pinMode(WIO_KEY_C, INPUT_PULLUP);
 
+  pinMode(WIO_5S_UP, INPUT_PULLUP);
+  pinMode(WIO_5S_DOWN, INPUT_PULLUP);
+  pinMode(WIO_5S_LEFT, INPUT_PULLUP);
+  pinMode(WIO_5S_RIGHT, INPUT_PULLUP);
+  pinMode(WIO_5S_PRESS, INPUT_PULLUP);
+
   pinMode(GSR, INPUT); // GSR
   Serial.begin(9600);
+  
+  
   tft.begin(); // start TFT LCD
   tft.setRotation(3); // set screen rotation
   tft.fillScreen(TFT_BLACK); // fill background
   tft.setTextColor(TFT_WHITE); // set text color
   tft.setTextSize(1); // set text size
-  spr.createSprite(TFT_HEIGHT - 50, TFT_WIDTH); //create sprite
+  spr.createSprite(TFT_HEIGHT , TFT_WIDTH); //create sprite
 
   /*INIZIALIZING SD CARD AND BATTERY*/
   tft.drawString("Sd card inzializing", 5, 2);
@@ -57,6 +77,29 @@ void setup() {
   }
   tft.drawString("Sd card ok", 5, 50);
   delay(2000);
+
+  listOfSessions = SD.open("listOfSessions.txt");
+  if (listOfSessions) {
+    // Leggi il file fino a quando non raggiungi la fine
+    while (listOfSessions.available()) {
+      // Leggi la stringa dalla riga del file e aggiungila all'array
+      String line = listOfSessions.readStringUntil('\n');
+      menuOfSessions[valueCount] = line;
+      valueCount++;
+     
+      // Puoi fare qualcos'altro con la stringa qui, se necessario
+    }
+    sessionsMax=valueCount;
+    Serial.println(sessionsMax);
+    delay(1000);
+    // Chiudi il file
+    listOfSessions.close();
+  } else {
+    // Se non riesci ad aprire il file, stampa un errore
+    Serial.println("Error openenig file");
+  }
+  
+  
   tft.fillScreen(TFT_BLACK); // fill background
 
   setupBQ27441();
@@ -66,6 +109,7 @@ void setup() {
   delay(1000);
   tft.fillScreen(TFT_BLACK); // fill background
   bar();
+  attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING); // start interrupt for HR
 
   /*END INIZIALIZING*/
 
@@ -73,6 +117,30 @@ void setup() {
 
 void loop() {
 
+
+
+   if (digitalRead(WIO_5S_LEFT) == LOW) {
+    sessionsPosition = sessionsPosition - 1;
+    delay(500);
+    if (sessionsPosition < 0) 
+    {
+      sessionsPosition = 0;
+    }
+    
+    bar();
+   }
+   
+  if (digitalRead(WIO_5S_RIGHT) == LOW) {
+    sessionsPosition = sessionsPosition + 1;
+    delay(500);
+    if (sessionsPosition > (sessionsMax-1)) 
+    {
+      sessionsPosition = (sessionsMax-1);
+    }
+     
+    bar();
+  }
+  
   if (digitalRead(WIO_KEY_C) == LOW) {
     serialSending = !serialSending;
     delay(500);
@@ -91,9 +159,13 @@ void loop() {
     while (!gsr_data.empty()) {
       gsr_data.pop(); // Remove last element
     }
+    while (!hr_data.empty()) {
+      hr_data.pop(); // Remove last element
+    }
     bar();
   }
   gsr();
+  //printGraph();
   printBatteryStats();
 
 }
@@ -148,7 +220,11 @@ void bar() {
     tft.setTextSize(1); // set text size
     tft.drawString("CSV:", 152, 2);
 
-   
+    //Session name
+    tft.fillRect(10, 50, 150, 20, TFT_BLACK);
+    tft.setTextSize(1); // set text size
+    tft.drawString(menuOfSessions[sessionsPosition], 10, 50);
+
    
    
 
@@ -206,27 +282,14 @@ void gsr() {
     gsrIndex = gsrIndex + 1;
     gsr_data.push(conductance);
 
-    // Initialize the sprite.
-    spr.fillSprite(black);
-
     if (gsr_data.size() == 300) gsr_data.pop();
-
     
 
-    //Draw graph
-    display_line_chart(20, "GSR->", TFT_HEIGHT - 50, 100, gsr_data, red, tft.color565(165, 40, 44));
-    spr.pushSprite(20, 100);
-    spr.setTextColor(red);
-    delay(50);
-
-    tft.setTextSize(2);
-
-    tft.setCursor(200, 128);
-    tft.print(conductance, 2); // draw float
-    tft.setCursor(100, 50);
-
     if (serialSending == true) {
-      
+      Serial.print("GSR");
+      Serial.print(",");
+      Serial.print(menuOfSessions[sessionsPosition]);
+      Serial.print(",");
       Serial.print(gsrIndex);
       Serial.print(",");
       Serial.print(currentMillis);
@@ -244,6 +307,8 @@ void gsr() {
 
         myFile.print("GSR");
         myFile.print(",");
+        myFile.print(menuOfSessions[sessionsPosition]);
+        myFile.print(",");
         myFile.print(gsrIndex);
         myFile.print(",");
         myFile.print(currentMillis);
@@ -253,7 +318,8 @@ void gsr() {
         myFile.close();
       }
 
-    } 
+    }
+    printGraph(); 
   }
 }
 
@@ -282,15 +348,13 @@ void printBatteryStats() {
   // Read battery stats from the BQ27441-G1A
 
   unsigned int soc = lipo.soc(); // Read state-of-charge (%)
-
-  Serial.print("PRE");
-  Serial.println(previousSoc);
-  Serial.print("SOC");
-  Serial.println(soc);
+  
+  delay(1); //necessary to make value printed
+  
   // Data
   if (previousSoc != soc) {
     tft.setTextSize(2);
-    tft.fillRect(250, 2, 250, 100, TFT_BLACK); // Cancella rettangolo 
+    tft.fillRect(250, 2, 250, 20, TFT_BLACK); // remove rectangle
     tft.setCursor(260, 2);
     tft.print(soc); // draw float
 
@@ -299,3 +363,98 @@ void printBatteryStats() {
   }
 
 }
+
+
+
+void interrupt() {
+  static unsigned long previousInterruptMillis = 0;
+  unsigned long currentInterruptMillis = millis();
+  if (currentInterruptMillis - previousInterruptMillis >= max_heartpluse_duty) {
+        
+    previousInterruptMillis = currentInterruptMillis;
+  } 
+  
+  else {
+    hrIndex++;
+    valueHeartRate = currentInterruptMillis - previousInterruptMillis;
+    previousInterruptMillis = currentInterruptMillis;
+    heartRate = 60000 / (valueHeartRate);
+    
+   
+    
+    hr_data.push(heartRate);
+    
+    if (hr_data.size() == 300) hr_data.pop();
+
+     if (serialSending == true) {
+      Serial.print("HR");
+      Serial.print(",");
+      Serial.print(menuOfSessions[sessionsPosition]);
+      Serial.print(",");
+      Serial.print(hrIndex);
+      Serial.print(",");
+      Serial.print(currentInterruptMillis);
+      Serial.print(",");
+      Serial.print(heartRate);
+      Serial.print(",");
+      Serial.println(valueHeartRate);
+      
+
+    } 
+    
+
+   if (recording == true) {
+     
+
+      /*write hr data in a CSV file*/
+      myFile = SD.open("hr.csv", FILE_APPEND);
+      if (myFile) {
+
+        myFile.print("HR");
+        myFile.print(",");
+        myFile.print(menuOfSessions[sessionsPosition]);
+        myFile.print(",");
+        myFile.print(hrIndex);
+        myFile.print(",");
+        myFile.print(currentInterruptMillis);
+        myFile.print(",");
+        myFile.print(heartRate);
+        myFile.print(",");
+        myFile.println(valueHeartRate);
+        // close the file:
+        myFile.close();
+      }
+
+    } 
+    
+  }
+ 
+}
+
+void printGraph(){
+    
+    
+    //Draw GSR graph
+    if (gsr_data.size()>0) {
+    spr.fillSprite(black);
+    display_line_chart(0, "GSR-", TFT_HEIGHT /2 - 10 , 100, gsr_data, red, tft.color565(165, 40, 44));
+    spr.pushSprite(0, 100);
+    tft.setTextSize(2);
+    tft.setCursor (102, 108);
+    tft.print(conductance, 2); // draw float
+    }
+    
+    //Draw HR graph
+    
+    if (hr_data.size()>0) {
+    spr.fillSprite(black);
+    display_line_chart(0, "HR-", TFT_HEIGHT /2 - 10 , 100, hr_data, red, tft.color565(165, 40, 44));
+    spr.pushSprite(TFT_HEIGHT /2, 100);
+    tft.setCursor (252, 108);
+    tft.print(heartRate, 0);
+    
+    }    
+        
+  
+  
+  }
